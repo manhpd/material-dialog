b53f31c0f0ee4a3ac7cf6a27521c688b9fd3d0f9
manhpd <manhpd.hut@gmail.com>
2024-01-03 10:41:02 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# material-dialog
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/manhpd/material-dialog)
---
+++ src/example/dialog-data-example.ts
@@ -0,0 +1,48 @@
+import {Component, Inject} from '@angular/core';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogTitle,
+  MatDialogContent,
+} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+
+export interface DialogData {
+  animal: 'panda' | 'unicorn' | 'lion';
+}
+
+/**
+ * @title Injecting data when opening a dialog
+ */
+@Component({
+  selector: 'dialog-data-example',
+  templateUrl: 'dialog-data-example.html',
+  standalone: true,
+  imports: [MatButtonModule],
+})
+export class DialogDataExample {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(DialogDataExampleDialog, {
+      data: {
+        animal: 'panda',
+      },
+    });
+  }
+}
+
+@Component({
+  selector: 'dialog-data-example-dialog',
+  templateUrl: 'dialog-data-example-dialog.html',
+  standalone: true,
+  imports: [MatDialogTitle, MatDialogContent],
+})
+export class DialogDataExampleDialog {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+}
+
+
+/**  Copyright 2024 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
