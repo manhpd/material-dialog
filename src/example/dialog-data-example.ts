e08508a4b02a63e0f905ba5b432c228c9d9e7629
manh <manhpd.hut@gmail>
2024-01-09 16:19:24 +0700

    Add toggle slider

--- src/example/dialog-data-example.ts
+++ src/example/dialog-data-example.ts
@@ -13,6 +13,8 @@ import {MatInputModule} from '@angular/material/input';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatSelectModule} from '@angular/material/select';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { NgIf } from '@angular/common';
 export interface DialogData {
   animal: 'panda' | 'unicorn' | 'lion';
 }
@@ -45,10 +47,18 @@ export class DialogDataExample {
   templateUrl: 'dialog-data-example-dialog.html',
   styleUrls: ['./dialog-data-example-dialog.scss'],
   standalone: true,
-  imports: [MatDialogTitle, MatDialogActions, MatDialogContent, MatFormFieldModule, MatInputModule, MatButtonModule, MatCheckboxModule, MatSelectModule, MatIconModule, MatDialogClose],
+  imports: [MatDialogTitle, MatDialogActions, NgIf, 
+    MatDialogContent, MatFormFieldModule, MatInputModule, MatButtonModule, 
+    MatCheckboxModule, MatSelectModule, MatIconModule, MatDialogClose, MatSlideToggleModule],
+    
 })
 export class DialogDataExampleDialog {
+  checked = false;
   constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+  onToggleChange(event: any) {
+    console.log(event);
+    this.checked = event.checked;
+  }
 }
 
 
